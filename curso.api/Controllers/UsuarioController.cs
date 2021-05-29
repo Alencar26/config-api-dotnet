8d32eb644968b652ac35a39c960a2639361fb042
Andre Alencar <aaugusto310@gmail.com>
2021-05-29 09:30:51 -0300

    configuração api - swagger, Annotations e ConfigureApiBehaviorOptions

---
+++ curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using curso.api.Filters;
+using curso.api.Models;
+using curso.api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Linq;
+
+namespace curso.api.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campo Obrigatório", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro Interno", Type = typeof(ErroGenericoViewModel))]
+        [HttpPost]
+        [Route("logar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        [ValidacaoModelStateCustomizado]
+        public IActionResult Registrar(RegristroViewModelInput regristroViewModelInput)
+        {
+            return Created("", regristroViewModelInput);
+        }
+    }
+}
